d6b3c92c19d08866a304dbb20ff85d4257764add
Bodabott <jiachin1995@gmail.com>
2019-09-27 11:33:36 +0800

    added more debugging statements

--- main.py
+++ main.py
@@ -60,6 +60,12 @@ class Main:
                 print("[@] Error reading instructions")
                 print(e)  
                 time.sleep(self.listen_rate)
+                
+                import sys, os
+                
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print(exc_type, fname, exc_tb.tb_lineno)
 
     def close_all_connections(self):
         #self.pc.close()
