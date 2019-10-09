bfeee5935173242301573e4919958409bef8c5de
Bodabott <jiachin1995@gmail.com>
2019-10-09 17:35:50 +0800

    added fastest path test

---
+++ testfastestpath.py
@@ -0,0 +1,35 @@
+from android import Android
+from arduino import Arduino
+from coordinator import Coordinator
+
+c = Coordinator()
+
+self.arduino = Arduino()
+self.arduino.connect()
+
+time.sleep(1)
+
+coordinator.arduino = arduino
+
+
+text = input("Press enter to start fastest path")
+
+
+#tests- run only one of them per test
+#test 1
+c.forward(5)            #1,6
+c.turnRight()
+c.forward(12)           #13,6
+c.turnLeft()            
+c.forward(12)           #13,18
+
+#test 2
+# c.forward(3)            #1,4
+# c.turnRight()
+# c.forward(3)            #4,4
+# c.turnLeft()
+# c.forward(5)            #4,9
+# c.turnRight()
+# c.forward(9)            #13,9
+# c.turnLeft()            
+# c.forward(9)             #13,18
