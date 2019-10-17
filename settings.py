86a33cabe7d262cf93fa33264953b6f27ec4ed53
Bodabott <jiachin1995@gmail.com>
2019-10-17 16:11:14 +0800

    changed settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
-
-comms = False                                #print communication logs
+comms = False                               #print communication logs
 logging = False                             #print logs statements, normally for debugging purposes
 mapformat_returntype_isHex = True           #default true. If false, returns map format as bits instead of hex
 optimise_diagonals = False                  #If True, optimise shortest path such that robot will make diagonal movements
@@ -13,7 +12,7 @@ right_sensors_position = 1                  #position of right sensor, front, mi
 """2 may not be debugged. Use after confirming that it works."""
 findallimages = 1                           #whether to find all images. 0 = no images, 1 = some images, 2 = all images
 images_threshold = 5                        #if findallimages>0, how many images to find before stopping image finding
-save_images = True                         #save image for every identification
+save_images = False                         #save image for every identification
 
 giveupspelunk = False                       #whether to give up on spelunk cause hardware cmi
 skipwhiteimages = True                      #whether to give up on white images cause image rec cmi
